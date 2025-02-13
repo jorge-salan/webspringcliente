4dbdaf9fc941870c2220f1ef70db028c57b99c25
Jorge Salan Perez <jorge.salan@enverus.com>
2025-02-13 13:12:34 +0100

    cliente inicio

---
+++ webcliente/src/main/java/com/arquitecturajava/webcliente/ClienteRestPersona.java
@@ -0,0 +1,26 @@
+package com.arquitecturajava.webcliente;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.arquitecturajava.webspring.dtos.PersonaDto;
+
+@RestController
+public class ClienteRestPersona {
+
+	@RequestMapping("/personascliente")
+	public List<PersonaDto> listarPersonas() {
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<PersonaDto[]> response = restTemplate.getForEntity("http://localhost:8083/personas",
+				PersonaDto[].class);
+		return Arrays.asList(response.getBody());
+
+	}
+
+}
